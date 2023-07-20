1a21e21e4fb83b23767892b9b8d7c5bcddab8384
stecrotti <sky_96@live.it>
2023-07-20 10:31:57 +0200

    orthogonalize

--- test/periodic_tensor_train.jl
+++ test/periodic_tensor_train.jl
@@ -58,6 +58,45 @@
         end
     end
 
+    @testset "Orthogonalize right" begin
+        svd_trunc = TruncThresh(3e-2)
+        q = 4; N = 3; L = 6
+        qs = fill(q, N)
+        A = uniform_periodic_tt( rand(1:20, L-1), qs... )
+        bd1 = bond_dims(A)
+        x = [rand(MersenneTwister(1234), 1:q[1],N) for _ in 1:L]
+        e1 = evaluate(A, x)
+        orthogonalize_right!(A; svd_trunc)
+        @assert sum(bond_dims(A)) < sum(bd1)
+        e2 = evaluate(A, x)
+        @test e1 ≈ e2
+    end
+
+    @testset "Orthogonalize left" begin
+        svd_trunc = TruncThresh(3e-2)
+        q = 4; N = 3; L = 6
+        qs = fill(q, N)
+        A = uniform_periodic_tt( rand(1:20, L-1), qs... )
+        bd1 = bond_dims(A)
+        x = [rand(MersenneTwister(1234), 1:q[1],N) for _ in 1:L]
+        e1 = evaluate(A, x)
+        orthogonalize_left!(A; svd_trunc)
+        @assert sum(bond_dims(A)) < sum(bd1)
+        e2 = evaluate(A, x)
+        @test e1 ≈ e2
+    end
+
+    @testset "Orthogonalize + sampling" begin
+        svd_trunc = TruncThresh(0.0)
+        q = 4; N = 3; L = 6
+        qs = fill(q, N)
+        A = uniform_periodic_tt( rand(1:20, L-1), qs... )
+        x1 = sample(MersenneTwister(1234), A)[1]
+        orthogonalize_left!(A; svd_trunc)
+        x2 = sample(MersenneTwister(1234), A)[1]
+        @test x1 == x2
+    end
+
 end
 
-
+nothing
