7f68718510419966e106008dcf60adcc63b34dcb
stecrotti <sky_96@live.it>
2023-09-26 10:24:09 +0200

    add infinite tensor train

--- src/uniform_tensor_train.jl
+++ src/uniform_tensor_train.jl
@@ -1,7 +1,16 @@
 """
-    UniformTensorTrain{F<:Number, N} <: AbstractPeriodicTensorTrain{F,N}
+    AbstractUniformTensorTrain{F,N} <: AbstractPeriodicTensorTrain{F,N}
 
-A type for representing a Tensor Train with periodic boundary conditions and all matrices equal
+An abstract type for representing tensor trains with periodic boundary conditions and all matrices equal
+- `F` is the type of the matrix entries
+- `N` is the number of indices of each tensor (2 virtual ones + `N-2` physical ones)
+    """
+abstract type AbstractUniformTensorTrain{F,N} <: AbstractPeriodicTensorTrain{F,N} end
+
+"""
+    UniformTensorTrain{F<:Number, N} <: AbstractUniformTensorTrain{F,N}
+
+A type for representing a tensor train with periodic boundary conditions, all matrices equal and a fixed length `L`.
 - `F` is the type of the matrix entries
 - `N` is the number of indices of each tensor (2 virtual ones + `N-2` physical ones)
 
@@ -9,14 +18,13 @@ A type for representing a Tensor Train with periodic boundary conditions and all
 - `tensor` only one is stored
 - `L` the length of the tensor train
 """
-struct UniformTensorTrain{F<:Number, N} <: AbstractPeriodicTensorTrain{F,N}
+struct UniformTensorTrain{F<:Number, N} <: AbstractUniformTensorTrain{F,N}
     tensor::Array{F,N}
     L :: Int
 
     function UniformTensorTrain{F,N}(tensor::Array{F,N}, L::Integer) where {F<:Number, N}
         N > 2 || throw(ArgumentError("Tensors shold have at least 3 indices: 2 virtual and 1 physical"))
-        size(tensor,1) == size(tensor,2) ||
-            throw(ArgumentError("Matrix must be square"))
+        size(tensor,1) == size(tensor,2) || throw(ArgumentError("Matrix must be square"))
         L > 0 || throw(ArgumentError("Length `L` must be positive, got $L"))
         return new{F,N}(tensor, Int(L))
     end
@@ -53,7 +61,7 @@ function Base.setindex!(::UniformTensorTrain, x, i::Integer)
 end
 
 # computes B = ∑ₓA(x)
-function one_normalization(A::UniformTensorTrain{F,N}) where {F,N}
+function one_normalization(A::AbstractUniformTensorTrain{F,N}) where {F,N}
     dims = tuple(3:N...)
     B = Matrix(dropdims(sum(A.tensor; dims); dims))
     return B
@@ -62,6 +70,12 @@ end
 function normalization(A::UniformTensorTrain; B = one_normalization(A))
     L = length(A)
     return tr(B^L)
+end
+
+function LinearAlgebra.normalize!(A::UniformTensorTrain)
+    Z = abs(normalization(A))
+    A.tensor ./= Z^(1/length(A))
+    return log(Z)
 end
 
 function marginals(A::UniformTensorTrain; B = one_normalization(A))
@@ -126,4 +140,67 @@ function symmetrized_uniform_tensor_train(A::AbstractTensorTrain)
         end
     end
     return UniformTensorTrain(tensor, length(A))
+end
+
+"""
+    InfiniteUniformTensorTrain{F<:Number, N} <: AbstractUniformTensorTrain{F,N}
+
+A type for representing an infinite tensor train with periodic boundary conditions and all matrices equal.
+- `F` is the type of the matrix entries
+- `N` is the number of indices of each tensor (2 virtual ones + `N-2` physical ones)
+
+## FIELDS
+- `tensor` only one is stored
+"""
+struct InfiniteUniformTensorTrain{F<:Number, N} <: AbstractUniformTensorTrain{F,N}
+    tensor::Array{F,N}
+
+    function InfiniteUniformTensorTrain{F,N}(tensor::Array{F,N}) where {F<:Number, N}
+        N > 2 || throw(ArgumentError("Tensors shold have at least 3 indices: 2 virtual and 1 physical"))
+        size(tensor,1) == size(tensor,2) || throw(ArgumentError("Matrix must be square"))
+        return new{F,N}(tensor)
+    end
+end
+function InfiniteUniformTensorTrain(tensor::Array{F,N}) where {F<:Number, N} 
+    return InfiniteUniformTensorTrain{F,N}(tensor)
+end
+
+Base.:(==)(A::T, B::T) where {T<:InfiniteUniformTensorTrain} = isequal(A.tensor, B.tensor)
+Base.isapprox(A::T, B::T; kw...) where {T<:InfiniteUniformTensorTrain} = isapprox(A.tensor, B.tensor; kw...)
+
+function _eigen(A::InfiniteUniformTensorTrain; B = one_normalization(A))
+    Q = eigen(B, sortby = λ -> (-abs(λ)))
+    r = eigvecs(Q) |> real
+    l = pinv(r)
+    λ = abs(eigvals(Q)[1])
+    l = l[1,:]
+    r = r[:,1]
+    λ, l, r
+end
+function normalization(A::InfiniteUniformTensorTrain; B = one_normalization(A))
+    λ, l, r = _eigen(A; B)
+    return λ * dot(l, r)
+end
+
+function LinearAlgebra.normalize!(A::InfiniteUniformTensorTrain)
+    Z = abs(normalization(A))
+    A.tensor ./= Z
+    return log(Z)
+end
+
+function Base.:(+)(A::InfiniteUniformTensorTrain{F,NA}, B::InfiniteUniformTensorTrain{F,NB}) where {F,NA,NB}
+    NA == NB || throw(ArgumentError("Tensor Trains must have the same number of variables, got $NA and $NB"))
+    sa = size(A.tensor); sb = size(B.tensor)
+    C = [ [A.tensor[:,:,x...] zeros(sa[1],sb[2]); zeros(sb[1],sa[2]) B.tensor[:,:,x...]] 
+                for x in Iterators.product(axes(A.tensor)[3:end]...)]
+    tensor = reshape( reduce(hcat, C), (sa .+ sb)[1:2]..., size(A.tensor)[3:end]...)
+    return InfiniteUniformTensorTrain(tensor)
+end
+
+function marginals(A::InfiniteUniformTensorTrain; B = one_normalization(A))
+    _, l, r = _eigen(A; B)
+    m = map(Iterators.product(axes(A.tensor)[3:end]...)) do x
+        l' * (@view A.tensor[:,:,x...]) * r
+    end
+    return [m / sum(m)]
 end
