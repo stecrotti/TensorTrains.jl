09a3515b28cee7123e51e64558de81640adceda0
stecrotti <sky_96@live.it>
2024-12-10 11:15:40 +0100

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using TensorTrains
 using TensorTrains: transfer_operator, leading_eig
 using Random, Suppressor, InvertedIndices, LinearAlgebra
 using Test
-using Random, Suppressor, InvertedIndices
 using Aqua
 
 @testset "Aqua" begin
