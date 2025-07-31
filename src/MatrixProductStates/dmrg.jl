5f754f5c89eac6184a5de44a1e5c79233705cf52
stecrotti <sky_96@live.it>
2025-07-31 17:40:11 +0200

    avoid orthogonalizing when not necessary

--- src/MatrixProductStates/dmrg.jl
+++ src/MatrixProductStates/dmrg.jl
@@ -10,17 +10,29 @@ function two_site_dmrg_sweep!(
     callback = (it, p, k, ll) -> nothing)
 
     for k in idxs
-        orthogonalize_two_site_center!(p, k; svd_trunc=TruncThresh(0.0))
+        # TODO: performance improvements
+        # The accumulation of matrices to the left and 
+        #  to the right (hidden in `grad_loglikelihood_two_site` -> `grad_evaluate_two_site`)
+        #  takes O(L). This should be avoidable by  memorizing stuff.
+        # Also, the checks on orthogonal form are O(L) and should eventually be dropped.
+        # Everything within this loop should eventually be O(1)
+
         for it in 1:ndesc
             A = _merge_tensors(p[k], p[k+1])
+            # Compute the gradient wrt the merged pair of tensors
             dlldA, ll = grad_loglikelihood_two_site(p, k, X)
-            p[k], p[k+1] = TensorTrains._split_tensor(A + η*dlldA; svd_trunc, lr)
+            # Do 1 step of gradient ascent
+            Anew = A + η*dlldA
+            # Split the updated tensor into two by SVD
+            p[k], p[k+1] = TensorTrains._split_tensor(Anew; svd_trunc, lr)
             callback(it, p, k, ll)
         end
     end
 end
 
 function two_site_dmrg!(p, X, nsweeps; kw...)
+    # Bring the MPS in canonical form wrt indices 1,2
+    orthogonalize_two_site_center!(p, 1; svd_trunc=TruncThresh(0.0))
     for sweep in 1:nsweeps
         two_site_dmrg_sweep!(p, X, 1:length(p)-1, Left(); kw...)
         two_site_dmrg_sweep!(p, X, length(p)-1:-1:1, Right(); kw...)
