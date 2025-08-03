c06aae9bad029d73e0f3e512fbd9b518a1de7c06
stecrotti <sky_96@live.it>
2025-08-03 11:10:45 +0200

    loglikelihood -> negative loglikelihood

--- src/MatrixProductStates/experiments_ising.jl
+++ src/MatrixProductStates/experiments_ising.jl
@@ -3,8 +3,9 @@
 using TensorTrains, TensorTrains.MatrixProductStates
 import UniformIsingModels
 using Random, LinearAlgebra, Unzip, Statistics
+using Plots
 
-N = 10
+N = 100
 J = 1.0
 rng = MersenneTwister(0)
 h = randn(rng, N)
@@ -14,27 +15,45 @@ nsamples = 5*10^3
 spin2int(σ) = 2 - (σ==-1)
 int2spin(x) = 2x-3
 S, ps = unzip([UniformIsingModels.sample(ising) for _ in 1:nsamples])
-X = [[[spin2int(σi)] for σi in σ] for σ in S]
-means_data = only.(mean(X))
+X = [[spin2int(σi) for σi in σ] for σ in S]
+means_data = mean(X)
 ll_data = mean(log, ps)
 println("Log-Likelihood according to generating distribution = $ll_data\n")
 
 p = MPS(rand_tt(ComplexF64, 2, N, 2))
 
 function CB()
+    lls = zeros(0)
+    ds = zeros(0)
+    dmax = zeros(0)
     function cb(it, p, k, ll)
         if it == 1
             means_p = [dot(eachindex(m), m) for m in marginals(p)]
             d_m = maximum(abs, means_data - means_p)
             mbd = maximum(bond_dims(p.ψ))
             println("# site k=$k")
-            println("it=$it.\nLogLikelihood=$ll\ndmax=$mbd")
+            println("LogLikelihood=$ll\ndmax=$mbd")
             println("Max diff empirical vs fitted means = $d_m\n")
+            push!(lls, ll)
+            push!(ds, d_m)
+            push!(dmax, mbd)
         end
     end
 end
 
 callback = CB()
-nsweeps = 2
+nsweeps = 5
 two_site_dmrg!(p, X, nsweeps; 
-    η=5e-2, ndesc=10, svd_trunc=TruncBond(2), callback)
+    η=5e-2, ndesc=1, svd_trunc=TruncBond(10), callback)
+
+println("Log-Likelihood according to generating distribution = $ll_data\n")
+
+pl = plot(; xlabel="Iterations")
+plot!(pl, callback.lls, label="Log-Likelihood")
+vline!(pl, (N-1):(N-1):length(callback.lls), label="Ends of sweeps", ls=:dash, c=:gray)
+hline!(pl, [ll_data], ls=:dot, c=:black, label="Log-Likelihood according to generating distr")
+pl2 = plot(callback.ds; label="Max difference in empirical vs fitted means", xlabel="Iterations")
+vline!(pl2, (N-1):(N-1):length(callback.lls), label="Ends of sweeps", ls=:dash, c=:gray, legend=:topright)
+pl3 = plot(callback.dmax, label="Max bond dim", xlabel="Iterations", legend=:bottomright)
+vline!(pl3, (N-1):(N-1):length(callback.lls), label="Ends of sweeps", ls=:dash, c=:gray)
+plot(pl, pl2, pl3, layout=(3,1), size=(700,500))
