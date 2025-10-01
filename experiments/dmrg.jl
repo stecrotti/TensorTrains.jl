91cb9c6724d0150a6e6f688e28b16e5aa47df74e
stecrotti <sky_96@live.it>
2025-10-01 19:49:15 +0200

    check whether complex MPS is learnable: not sure

--- experiments/dmrg.jl
+++ experiments/dmrg.jl
@@ -1,18 +1,22 @@
 using TensorTrains, TensorTrains.MatrixProductStates
+using Plots
 
 d_original = 3
 L = 5
-q = rand_mps(ComplexF64, d_original, L, 2,2)
+q = rand_mps(ComplexF64, d_original, L, 4)
 normalize!(q)
 nsamples = 5*10^3
 X = [sample(q)[1] for _ in 1:nsamples]
 nll = -loglikelihood(q, X)
-println("Negative Log-Likelihood according to generating distribution q=$nll\n")
+println("Negative Log-Likelihood according to generating distribution q = $nll\n")
 mq = marginals(q)
 
-p = rand_mps(Float64, 2, length(q), 2,2)
+p = rand_mps(ComplexF64, 2, length(q), 4)
 
 function CB()
+    nlls = zeros(0)
+    diff_marg = zeros(0)
+    dots = zeros(0)
     function cb(sweep, k, p, nll)
         p_cp = deepcopy(p)
         normalize!(p_cp)
@@ -21,13 +25,25 @@ function CB()
         mp = marginals(p)
         d_m = maximum(maximum.(abs, mp-mq))
         println("# Sweep $sweep, site k=$k")
-        println("Negative LogLikelihood=$nll.\n\tdmax=$mbd")
+        println("Negative LogLikelihood = $nll.\ndmax=$mbd")
         println("|<p|q>|=$d")
-        println("Max diff marginals = $d_m")
+        println("Max diff marginals = $d_m\n")
+        push!(nlls, -loglikelihood(p, X))
+        push!(diff_marg, d_m)
+        push!(dots, d)
     end
 end
 
 callback = CB()
-nsweeps = 4
+nsweeps = 40
 two_site_dmrg!(p, X, nsweeps;
-    η=1e-3, ndesc=10, svd_trunc=TruncBond(d_original), callback)
+    η=1e-4, ndesc=10, svd_trunc=TruncBond(d_original+2), callback)
+
+pl1 = plot(callback.nlls, xlabel="it", ylabel="NLL", label="")
+hline!(pl1, [nll], ls=:dash, c=:gray, label="NLL according to generative model")
+# vline!(pl1, (N-1):(N-1):length(callback.nlls), label="Ends of sweeps", ls=:dash, c=:gray)
+pl2 = plot(callback.diff_marg, xlabel="it", ylabel="Max error on marginals", label="")
+# vline!(pl2, (N-1):(N-1):length(callback.nlls), label="Ends of sweeps", ls=:dash, c=:gray)
+pl3 = plot(callback.dots, xlabel="it", ylabel="<p|q>", label="")
+# vline!(pl3, (N-1):(N-1):length(callback.nlls), label="Ends of sweeps", ls=:dash, c=:gray)
+plot(pl1, pl2, pl3, layout=(3,1), size=(500,800), margin=5Plots.mm)
